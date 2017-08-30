06e12a5471614eed344b4f85478f16225dd26b9a
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-08-31 03:46:33 +0800

    made alpha and gamma parameters modifiable

--- chase_exp.py
+++ chase_exp.py
@@ -7,22 +7,29 @@ from agents import Mouse
 from agents import Cat
 
 if __name__ == '__main__':
-
-    env = Environment(world=World(map='worlds/waco.txt', Cell=Casual))
     
-    mouse = Mouse()
-    env.add_agent(mouse)
-    env.world.mouse = mouse
-    
-    cat = Cat()
-    env.add_agent(cat)
-    env.world.cat = cat
-    
-    cheese = Cheese()
-    env.add_agent(cheese)
-    env.world.cheese = cheese
-    
-    # for 10_000 training ages
-    for _ in range(1000000):
-        env.update()
+    for alpha in range(1, 10):
+        for gamma in range(1, 10):
+            env = Environment(world=World(map='worlds/waco.txt', Cell=Casual))
+            
+            mouse = Mouse()
+            env.add_agent(mouse)
+            mouse.ai.alpha = alpha/10
+            mouse.ai.gamma = gamma/10
+            env.world.mouse = mouse
+            
+            cat = Cat()
+            env.add_agent(cat)
+            env.world.cat = cat
+            
+            cheese = Cheese()
+            env.add_agent(cheese)
+            env.world.cheese = cheese
+            
+            #env.show()
+            
+            for i in range(100000):
+                env.update()
+                if i == 99999:
+                    print(alpha/10, gamma/10, env.world.eaten, env.world.fed)
     
