c20c3d3413c0cf743df9a52cee5404e6c242ea3b
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-02 11:17:32 +0800

    finished experiment 3

--- chase_exp.py
+++ chase_exp.py
@@ -7,16 +7,14 @@ import agents
 import time
 import multiprocessing
 
-#training_ages = 10**3
-trials = 500
+trials = 1000
 steps = 10
-runs = 5
+runs = 10
 
 def worker(params):
     start = time.time()
     
     alpha, gamma = params
-    #print("start:", alpha, gamma, training_age)
     env = Environment(world=World(map='worlds/empty10x10.txt', Cell=Casual))
     
     mouse = agents.Mouse()
@@ -47,15 +45,8 @@ def worker(params):
         if env.world.fed is not prev_fed and (env.world.fed + 1) % steps == 0:
             data.append(env.world.age)
         prev_fed = env.world.fed
-    #global training_ages
-    #for i in range(training_ages):
-    #    env.update()
-    
-    #print(data)
+
     return (alpha, gamma, tuple(data))
-    
-    #end = time.time()
-    #print(alpha/10, gamma/10, env.world.eaten, env.world.fed, end - start)
 
 def to_text(data):
     text = ""
@@ -66,9 +57,7 @@ def to_text(data):
 if __name__ == '__main__':
     for depth in range(1, 5):
         agents.visual_depth = depth
-        #agents.calc_lookcells()
         print("visual depth:", agents.visual_depth)
-        #print(agents.lookcells)
         for run in range(1, runs + 1):
             start = time.time()
             
