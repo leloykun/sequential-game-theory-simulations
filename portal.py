33cbbbf680b0cb48d08061b1f0643315d586a014
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-06 00:39:25 +0800

    started centralizing experiments

---
+++ portal.py
@@ -0,0 +1,23 @@
+import os, sys, inspect
+import importlib
+
+import cell
+import agent
+
+# add current directory to pythonpath
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+# get list of simulations
+sims_list = sys.argv[1] if len(sys.argv) > 1 else 'sims/list.txt'
+
+if __name__ == '__main__':
+    # run each simulation
+    with open(sims_list) as f:
+        sims_list = f.readlines()
+        for sim in sims_list:
+            sim = sim[:-1]
+            sim = importlib.import_module('sims.' + sim + '.' + sim)
+            # TODO get params from list
+            sim.run(100, 10, 10)
---
+++ sims/simple-migration/simple-migration.py
@@ -0,0 +1,5 @@
+def run():
+    print("simple-migration")
+
+if __name__ == '__main__':
+    run()
