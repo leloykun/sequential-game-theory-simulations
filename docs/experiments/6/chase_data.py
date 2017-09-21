9a2729a89c9c2fd7d662259ac9240425bb14139e
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-21 14:19:41 +0800

    added vertical lines to graphs
    vertical lines indicate whenever the agent encounters a new state

--- docs/experiments/6/chase_data.py
+++ docs/experiments/6/chase_data.py
@@ -14,13 +14,22 @@ for r in range(1, runs+1):
             time.append(i+1)
             data[0].append(temp[i][0])
             data[1].append(temp[i][1])
-        
-    # print(len(data[1]))
-    # print(data[1])
-        
+
+    num_states = []
+    with open(str(r) + "states.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.split()
+            # print(line)
+            if line[0] == "states:":
+                num_states.append(int(line[1]))
+    for xc in range(1, 1000):
+        if num_states[xc] != num_states[xc - 1]:
+            pl.axvline(x=xc, color='pink', linestyle='-', linewidth=1)
+
     plot1 = pl.plot(time, data[0], c='#6677AA')
     plot2 = pl.plot(time, data[1], c='#0000FF')
-    
+
     pl.title('Residual Entropy over Time')
 
     pl.xlabel('training age')
@@ -30,8 +39,6 @@ for r in range(1, runs+1):
 
     pl.xlim(0, 1001)
     pl.ylim(0.0, 1.0)
-
-    #pl.xticks(np.arange(0, 10000+1, 100))
 
     pl.tight_layout()
     pl.savefig(str(r) + 'plot-both')
