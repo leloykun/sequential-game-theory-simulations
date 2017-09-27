7a661d3b450d3a0efa74e5c2428671f735c91a9c
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-27 18:31:01 +0800

    improved graphs in experiment 6

--- docs/experiments/6/chase_data.py
+++ docs/experiments/6/chase_data.py
@@ -15,7 +15,7 @@ for r in range(1, runs+1):
             data[0].append(temp[i][0])
             data[1].append(temp[i][1])
 
-    num_states = []
+    '''num_states = []
     with open(str(r) + "states.txt") as f:
         lines = f.readlines()
         for line in lines:
@@ -25,15 +25,15 @@ for r in range(1, runs+1):
                 num_states.append(int(line[1]))
     for xc in range(1, 1000):
         if num_states[xc] != num_states[xc - 1]:
-            pl.axvline(x=xc, color='pink', linestyle='-', linewidth=1)
+            pl.axvline(x=xc, color='pink', linestyle='-', linewidth=1)'''
 
-    plot1 = pl.plot(time, data[0], c='#6677AA')
+    #plot1 = pl.plot(time, data[0], c='#6677AA')
     plot2 = pl.plot(time, data[1], c='#0000FF')
 
     pl.title('Residual Entropy over Time')
 
-    pl.xlabel('training age')
-    pl.ylabel('residual entropy')
+    pl.xlabel('Time')
+    pl.ylabel('Residual Entropy')
 
     pl.xticks(np.arange(0, 1001+1, 100.0))
 
@@ -41,5 +41,5 @@ for r in range(1, runs+1):
     pl.ylim(0.0, 1.0)
 
     pl.tight_layout()
-    pl.savefig(str(r) + 'plot-both')
+    pl.savefig(str(r) + 'plot')
     pl.close()
