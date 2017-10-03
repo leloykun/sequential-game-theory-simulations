c6fdec7fc526cd4492da268a8c9eec7afe8afe1e
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-04 05:46:55 +0800

    added steps in linear regression

--- sims/cat-mouse/data_analyzer.py
+++ sims/cat-mouse/data_analyzer.py
@@ -5,21 +5,26 @@ def analyze(trials=100, steps=10, runs=10):
     layers = int(trials / steps)
     X = []
     Y = []
-    
+
     for depth in range(1, 5):
         for run in range(1, runs + 1):
             with open("data/" + str(depth) + "/data" + str(run) + ".txt") as f:
                 for line in f.readlines():
                     line = list(map(int, line.split()))
-                    X.append([depth, line[0], line[1]])
-                    Y.append(line[-1])
-    
-    clf = linear_model.LinearRegression()
+                    for i in range(2, 12):
+                        X.append([(i - 1) * steps, depth, line[0]/10, line[1]/10])
+                        Y.append(line[i])
+
+    clf = linear_model.LinearRegression(n_jobs=-1)
     clf.fit(X, Y)
-    
-    print(str(clf.intercept_) + " + " + str(clf.coef_[0]) + "*depth + " + str(clf.coef_[1]) + "*learning_rate + " + str(clf.coef_[2]) + "*discount_rate")
-    # print(clf.coef_)
-    # print(clf.intercept_)
+
+    print(str(round(clf.intercept_, 2)) + " + " + \
+          str(round(clf.coef_[0], 2)) + "*step + " + \
+          str(round(clf.coef_[1], 2)) + "*depth + " + \
+          str(round(clf.coef_[2], 2)) + "*alpha + " + \
+          str(round(clf.coef_[3], 2)) + "*gamma")
+
+    print("R-squared:", str(round(clf.score(X, Y), 4)))
 
 if __name__ == '__main__':
     analyze()
