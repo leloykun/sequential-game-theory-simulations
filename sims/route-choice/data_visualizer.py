80248a68d968575a8a2e742ea9e51392b467771f
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-03 22:17:30 +0800

    added linear regression stuff in cat-mouse

--- sims/route-choice/data_visualizer.py
+++ sims/route-choice/data_visualizer.py
@@ -6,7 +6,7 @@ def visualize(runs=10):
     for run in range(1, runs + 1):
         data = [[], [], [], []]
         time = []
-        
+
         with open('data/dist/' + str(run) + 'run.txt') as f:
             for line in f.readlines():
                 a, b, c, d = map(int, line.split())
@@ -15,21 +15,16 @@ def visualize(runs=10):
                 data[1].append(b)
                 data[2].append(c)
                 data[3].append(d)
-        
-        '''with sns.color_palette("PuBuGn_d"):
-            for i in range(4):
-                # sns.tsplot(data=data[i])
-                plt.plot(time, data[i])'''
-                
+
         fig, ax = plt.subplots()
-        
+
         plt.xlim(1, 1000)
         plt.ylim(1, 50)
-        
+
         time = np.array(time)
         for i in range(4):
             sns.regplot(x=time[:1000], y=np.array(data[i][:1000]), ax=ax, ci=100, marker="+", n_boot=10000, scatter=False)
-        
+
         plt.savefig('data/dist/' + str(run) + 'plot1K')
         plt.close()
         print("done with run %d" % run)
