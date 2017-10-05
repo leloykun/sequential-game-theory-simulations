f0ba039a901f32936ec8a5e9da691322c5af7fe3
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-05 10:28:48 +0800

    added logistic regression

--- sims/cat-mouse/data_analyzer.py
+++ sims/cat-mouse/data_analyzer.py
@@ -1,13 +1,16 @@
+import gc
 import time
 from sklearn import linear_model
 
 def analyze(trials=100, steps=10, runs=10):
+    start = time.time()
+
     layers = int(trials / steps)
     X = []
     Y = []
 
     for depth in range(1, 5):
-        for run in range(1, runs + 1):
+        for run in range(1, 1 + 1):
             with open("data/" + str(depth) + "/data" + str(run) + ".txt") as f:
                 for line in f.readlines():
                     line = list(map(int, line.split()))
@@ -15,16 +18,38 @@ def analyze(trials=100, steps=10, runs=10):
                         X.append([(i - 1) * steps, depth, line[0]/10, line[1]/10])
                         Y.append(line[i])
 
-    clf = linear_model.LinearRegression(n_jobs=-1)
-    clf.fit(X, Y)
+    '''lin_reg = linear_model.LinearRegression(n_jobs=-1)
+    lin_reg.fit(X, Y)
 
-    print(str(round(clf.intercept_, 2)) + " + " + \
-          str(round(clf.coef_[0], 2)) + "*step + " + \
-          str(round(clf.coef_[1], 2)) + "*depth + " + \
-          str(round(clf.coef_[2], 2)) + "*alpha + " + \
-          str(round(clf.coef_[3], 2)) + "*gamma")
+    print(str(round(lin_reg.intercept_, 2)) + " + " + \
+          str(round(lin_reg.coef_[0], 2)) + "*step + " + \
+          str(round(lin_reg.coef_[1], 2)) + "*depth + " + \
+          str(round(lin_reg.coef_[2], 2)) + "*alpha + " + \
+          str(round(lin_reg.coef_[3], 2)) + "*gamma")
 
-    print("R-squared:", str(round(clf.score(X, Y), 4)))
+    print("R-squared:", str(round(lin_reg.score(X, Y), 4)))
+    print()'''
+
+    print(len(X))
+    log_reg = linear_model.LogisticRegression(verbose=10, C=1e10, tol=1e-7, max_iter=1000)
+    log_reg.fit(X, Y)
+    print()
+
+    # print(log_reg.coef_)
+    # print(log_reg.intercept_)
+
+    with open('data/coef.txt', 'w') as f:
+        f.write('\n'.join(' '.join(map(str, line)) for line in log_reg.coef_))
+    with open('data/intercept.txt', 'w') as f:
+        f.write(' '.join(map(str, log_reg.intercept_)))
+    with open('data/r-squared.txt', 'w') as f:
+        f.write(str(log_reg.score(X, Y)))
+
+    print("R-squared:", round(log_reg.score(X, Y), 4))
+    X_test = [[0, 1, 0.5, 0.5], [50, 1, 0.5, 0.5], [100, 1, 0.5, 0.5]]
+    print(log_reg.predict(X_test))
+
+    print("time taken:", time.time() - start)
 
 if __name__ == '__main__':
     analyze()
