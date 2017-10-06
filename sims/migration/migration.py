21866b9fbf34edfb93d285ad70698569e3fe221e
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-06 20:47:32 +0800

    copied cat-mouse-cheese to migration as template

--- sims/migration/migration.py
+++ sims/migration/migration.py
@@ -12,6 +12,8 @@ from environment import Environment
 sim_name = 'migration'
 output_dir = 'sims/' + sim_name + '/data/'
 
+max_visual_depth = 4
+
 
 class CasualCell(Cell):
     wall = False
@@ -32,24 +34,68 @@ class CasualCell(Cell):
         return len(self.agents)
 
 
+class Cheese(Agent):
+    colour = 'yellow'
+
+    def update(self):
+        if self.move:
+            cell = self.cell
+            while cell == self.cell:
+                self.goInDirection(random.randrange(8))
+
+
 class Mouse(Agent):
-    colour = 'grey'
+    colour = 'gray'
+    visual_depth = 2
+    lookcells = []
 
     def __init__(self):
-        self.ai = QLearn(actions=list(range(8)))
+        self.ai = QLearn(
+            actions=list(range(8)),
+            temp=5,
+            alpha=0.5,
+            gamma=0.5,
+            epsilon=0.1)
         self.ai.agent = self
 
         self.eaten = 0
         self.fed = 0
-        self.score = 0
 
         self.last_action = None
         self.last_state = None
 
+        self.last_action = None
+        self.last_state = None
+
+        self.calc_lookcells()
+
+    def calc_lookcells(self):
+        self.lookcells = []
+        for i in range(-self.visual_depth, self.visual_depth + 1):
+            for j in range(-self.visual_depth, self.visual_depth + 1):
+                self.lookcells.append((i, j))
+
     def update(self):
         state = self.calc_state()
-        reward = state[0]
-        self.score += reward
+        reward = -1
+
+        if self.cell == self.world.cat.cell:
+            self.eaten += 1
+            reward = -100
+            if self.last_state is not None:
+                self.ai.learn(self.last_state,
+                              self.last_action,
+                              reward,
+                              state)
+
+            self.last_state = None
+            self.cell = self.env.get_random_avail_cell()
+            return
+
+        if self.cell == self.world.cheese.cell:
+            self.fed += 1
+            reward = 50
+            self.world.cheese.cell = self.env.get_random_avail_cell()
 
         if self.last_state is not None:
             self.ai.learn(self.last_state,
@@ -57,62 +103,99 @@ class Mouse(Agent):
                           reward,
                           state)
 
+        state = self.calc_state()
         action = self.ai.chooseAction(state)
+
         self.last_state = state
         self.last_action = action
 
         self.goInDirection(action)
 
     def calc_state(self):
-        day = self.world.age % 100
-        if day < 50:
-            return (self.cell.y - 10, 0)
-        else:
-            return (11 - self.cell.y, 1)
+        cat = self.world.cat
+        cheese = self.world.cheese
+
+        def cell_value(cell):
+            if cat.cell is not None and (cell.x == cat.cell.x and
+                                         cell.y == cat.cell.y):
+                return 3
+            elif cheese.cell is not None and (cell.x == cheese.cell.x and
+                                              cell.y == cheese.cell.y):
+                return 2
+            elif cell.wall:
+                return 1
+            else:
+                return 0
+
+        return tuple([
+            cell_value(self.world.get_wrapped_cell(self.cell.x + j, 
+                                                   self.cell.y +i))
+            for i, j in self.lookcells
+        ])
+
+    def going_to_obstacle(self, action):
+        cell = self.world.getPointInDirection(self.cell.x,
+                                              self.cell.y,
+                                              action)
+        cell = self.world.get_cell(cell[0], cell[1])
+        return cell.wall or cell.num_agents > 0
+
+
+class Cat(Agent):
+    colour = 'red'
+
+    def update(self):
+        cell = self.cell
+        if cell != self.world.mouse.cell:
+            self.goTowards(self.world.mouse.cell)
+            while cell == self.cell:
+                self.goInDirection(random.randrange(self.world.num_dir))
 
 
 def worker(params):
-    alpha, gamma, temp_power, timesteps, run = params
+    alpha, gamma, timesteps, interval = params
 
-    env = Environment(world=World(map='worlds/box20x10.txt',
-                                  Cell=CasualCell))
+    env = Environment(world=World(map='worlds/waco.txt', Cell=CasualCell))
 
     mouse = Mouse()
     env.add_agent(mouse)
     mouse.ai.alpha = alpha / 10
     mouse.ai.gamma = gamma / 10
-    mouse.ai.temp = 2**temp_power
+    mouse.ai.temp = 0.5
     env.world.mouse = mouse
 
-    scores = []
-    positions = []
-    res_ent = []
-    num_states = []
+    cat = Cat()
+    env.add_agent(cat)
+    env.world.cat = cat
+
+    cheese = Cheese()
+    env.add_agent(cheese)
+    cheese.move = False
+    env.world.cheese = cheese
 
     # env.show()
 
+    losses = []
+    wins = []
     for now in range(1, timesteps + 1):
-        env.update(0, mouse.score)
+        env.update(mouse.eaten, mouse.fed)
 
-        scores.append(mouse.score)
-        positions.append(mouse.cell.y)
-        res_ent.append(
-            str(mouse.ai.stat_ARE) + " " + str(mouse.ai.dyna_ARE))
-        num_states.append(len(mouse.ai.states))
+        if now % interval == 0:
+            losses.append(mouse.eaten)
+            wins.append(mouse.fed)
 
-    output_dir_dir = output_dir + str(temp_power) + "/" + str(run)
+    losses = " ".join(map(str, losses))
+    wins = " ".join(map(str, wins))
 
-    with open(output_dir_dir + "scores.txt", 'w') as f:
-        f.write(' '.join(map(str, scores)))
+    return str(alpha) + " " + str(gamma) + " " + losses + " " + wins
 
-    with open(output_dir_dir + "pos.txt", 'w') as f:
-        f.write(' '.join(map(str, positions)))
 
-    with open(output_dir_dir + "res_ent.txt", 'w') as f:
-        f.write('\n'.join(map(str, res_ent)))
-
-    with open(output_dir_dir + "num_states.txt", 'w') as f:
-        f.write(' '.join(map(str, num_states)))
+def ord(n):
+    return str(n) + ("th" if 4 <= n % 100 <= 20 else {
+        1: "st",
+        2: "nd",
+        3: "rd"
+    }.get(n % 10, "th"))
 
 
 def process(params):
@@ -120,20 +203,32 @@ def process(params):
 
 
 def run(params):
-    runs, timesteps, temp_powers = process(params)
+    runs, timesteps, interval = process(params)
 
-    print("cat-mouse-cheese starting...")
+    print("migration starting...")
     print("runs = %d,  timesteps = %d" % (runs, timesteps))
     sim_start = time.time()
 
-    params = []
-    for run in range(1, runs + 1):
-        for power in range(-temp_powers, temp_powers + 1):
-            params.append((0.5, 0.5, power, timesteps, run))
+    for depth in range(1, max_visual_depth + 1):
+        Mouse.visual_depth = depth
+        print("   visual depth:", Mouse.visual_depth)
 
-    with multiprocessing.Pool(4) as pool:
-        pool.map(func=worker, iterable=params)
+        for run in range(1, runs + 1):
+            run_start = time.time()
 
-    print("cat-mouse-cheese finished...")
+            params = []
+            for alpha in range(11):
+                for gamma in range(11):
+                    params.append((alpha, gamma, timesteps, interval))
+
+            with multiprocessing.Pool(4) as pool:
+                results = pool.map(worker, params)
+
+            with open(output_dir + str(depth) + '/data' + str(run) + '.txt', 'w') as f:
+                f.write("\n".join(results))
+
+            print("     ", ord(run), "runtime:", time.time() - run_start, "secs")
+
+    print("migration finished...")
     print("overall runtime:", time.time() - sim_start, "secs")
     print()
