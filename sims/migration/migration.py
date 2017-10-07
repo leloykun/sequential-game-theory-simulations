5ec442731d00e7ad2dcb444368340f8df02161dd
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-07 23:37:34 +0800

    used relative imports in migration

--- sims/migration/migration.py
+++ sims/migration/migration.py
@@ -4,11 +4,11 @@ import math
 import random
 import multiprocessing
 
-from cell import Cell
-from agent import Agent
-from world import World
-from qlearn import QLearn
-from environment import Environment
+from ...cell import Cell
+from ...agent import Agent
+from ...world import World
+from ...qlearn import QLearn
+from ...environment import Environment
 
 sim_name = 'migration'
 output_dir = 'sims/' + sim_name + '/data/'
