1030faba3950d7bfe9a47337b0e91df87a9cd613
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-10 21:25:02 +0800

    renamed Prey to DumbPrey

--- sims/cat_mouse/cat_mouse.py
+++ sims/cat_mouse/cat_mouse.py
@@ -7,7 +7,7 @@ from ...agent import Agent
 from ...world import World
 from ...qlearn import QLearn
 from ...cell import CasualCell
-from ...agent import Prey as Mouse
+from ...agent import DumbPrey as Mouse
 from ...environment import Environment
 
 sim_name = 'cat_mouse'
