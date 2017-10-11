3718a9ad14c3514a732ba87a4757e603751d1929
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-12 07:22:32 +0800

    added testing of data analyzers and data visualizers

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,2 +1,13 @@
-def test_function():
-    assert 1 == 1
+from ..sims.cat_mouse import data_analyzer as cat_mouse_ana
+
+from ..sims.cat_mouse import data_visualizer as cat_mouse_vis
+from ..sims.route_choice import data_visualizer as route_choice_vis
+from ..sims.simple_migration import data_visualizer as simple_mig_vis
+
+def test_data_analyzers():
+    cat_mouse_analyzer.analyze()
+
+def test_data_visualizers():
+    cat_mouse_visualizer.visualize()
+    route_choice_visualizer.visualize()
+    simple_mig_vis.visualize()
