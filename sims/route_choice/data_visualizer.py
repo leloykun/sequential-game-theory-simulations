10ca459c6da5f32eafbfdb9a014d3bb260a209b0
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-12 20:21:37 +0800

    speed up the tests

--- sims/route_choice/data_visualizer.py
+++ sims/route_choice/data_visualizer.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def visualize(runs=10):
+def visualize(runs=10, test=False):
+    n_boot_ = 100 if test else 10000
+    ci_ = 50 if test else 100
+    plot_till = 100 if test else 1000
+
     for run in range(1, runs + 1):
         data = [[], [], [], []]
         time = []
@@ -23,17 +27,17 @@ def visualize(runs=10):
 
         _, ax = plt.subplots()
 
-        plt.xlim(1, 1000)
+        plt.xlim(1, plot_till)
         plt.ylim(1, 50)
 
         time = np.array(time)
         for i in range(4):
-            sns.regplot(x=time[:1000],
-                        y=np.array(data[i][:1000]),
+            sns.regplot(x=time[:plot_till],
+                        y=np.array(data[i][:plot_till]),
                         ax=ax,
-                        ci=100,
+                        ci=ci_,
                         marker="+",
-                        n_boot=10000,
+                        n_boot=n_boot_,
                         scatter=False)
 
         dir_output = 'data/dis/' + str(run) + 'plot1K.png'
