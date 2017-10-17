d5cabde4d08c74c4c5d06546c39a952d805cd769
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-17 09:52:44 +0800

    removed tests for cat_mouse_analyzer

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,12 +1,6 @@
-from ..sims.cat_mouse import data_analyzer as cat_mouse_ana
-
 from ..sims.cat_mouse import data_visualizer as cat_mouse_vis
 from ..sims.route_choice import data_visualizer as route_choice_vis
 from ..sims.simple_migration import data_visualizer as simple_mig_vis
-
-
-def test_cat_mouse_analyzer():
-    cat_mouse_ana.analyze(test=True)
 
 
 def test_cat_mouse_visualizer():
