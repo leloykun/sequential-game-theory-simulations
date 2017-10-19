de5df804e033303174270e1f461783b17f0427c1
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-19 21:50:14 +0800

    disabled basic_tests.py

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,8 +1,8 @@
-from ..sims import data_visualizer as cat_mouse_vis
-from ..sims import data_visualizer as route_choice_vis
-from ..sims import data_visualizer as simple_mig_vis
+# from ..sims.deprecated.cat_mouse import data_visualizer as cat_mouse_vis
+# from ..sims.deprecated.route_choice import data_visualizer as route_choice_vis
+# from ..sims.deprecated.simple_migration import data_visualizer as simple_mig_vis
 
-
+"""
 @pytest.mark.skip(reason="deprecated")
 def test_cat_mouse_visualizer():
     cat_mouse_vis.visualize(test=True)
@@ -16,3 +16,4 @@ def test_route_choice_visualizer():
 @pytest.mark.skip(reason="deprecated")
 def test_simple_migration_visualizer():
     simple_mig_vis.visualize(test=True)
+"""
