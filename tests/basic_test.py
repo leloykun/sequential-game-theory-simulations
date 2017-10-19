1730154545f04f431abdd631393227b4f59d15a0
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-10-19 21:45:24 +0800

    moved deprecated files to deprecated/

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,15 +1,18 @@
-from ..sims.cat_mouse import data_visualizer as cat_mouse_vis
-from ..sims.route_choice import data_visualizer as route_choice_vis
-from ..sims.simple_migration import data_visualizer as simple_mig_vis
+from ..sims import data_visualizer as cat_mouse_vis
+from ..sims import data_visualizer as route_choice_vis
+from ..sims import data_visualizer as simple_mig_vis
 
 
+@pytest.mark.skip(reason="deprecated")
 def test_cat_mouse_visualizer():
     cat_mouse_vis.visualize(test=True)
 
 
+@pytest.mark.skip(reason="deprecated")
 def test_route_choice_visualizer():
     route_choice_vis.visualize(test=True)
 
 
+@pytest.mark.skip(reason="deprecated")
 def test_simple_migration_visualizer():
     simple_mig_vis.visualize(test=True)
